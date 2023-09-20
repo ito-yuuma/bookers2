d4886c101efe7064e86582c7259a0c68e2b6b948
EC2 Default User <ec2-user@ip-172-31-5-255.ap-northeast-1.compute.internal>
2023-09-20 09:03:50 +0000

    halfway

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,18 @@
+class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+
+     @book = Book.new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destory
+  end
+end
